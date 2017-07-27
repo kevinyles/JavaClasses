e3d256855f48908a9aa6a1861c04e7c38f35c2df
kevinyles <kevinyles@gmail.com>
2017-07-28 05:03:18 +0800

    for work

---
+++ p002/src/p002/helloer.java
@@ -0,0 +1,19 @@
+package p002;
+
+public class helloer {
+	
+
+	public static void sayHello() {
+		
+		
+		System.out.println("I'm a helloer.");
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		        sayHello();
+	}
+
+}
