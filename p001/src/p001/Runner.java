c1557293e96d7a2fdc538506c2d204b2ef29ef95
kevinyles <30422530+kevinyles@users.noreply.github.com>
2017-07-28 05:37:10 +0800

    Update Runner.java

--- p001/src/p001/Runner.java
+++ p001/src/p001/Runner.java
@@ -3,7 +3,7 @@ package p001;
 public class Runner {
 	public static void main(String[] args) {
 		
-		Helloer runHelloer=new Helloer();
-		runHelloer.sayHello();
+		Helloer rH=new Helloer();
+		rH.sayHello();
 	}
 }
